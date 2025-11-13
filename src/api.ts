482e26f487d62bee753ab31816b293d9881754a0
hugo-lrg <143999231+hugo-lrg@users.noreply.github.com>
2025-11-13 16:11:42 +0100

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // will call relative endpoints like /authors, /books, etc.
+  baseURL: 'https://tp-cicd-master.onrender.com:443', // will call relative endpoints like /authors, /books, etc.
 });
 
 export default api;
